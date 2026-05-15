067e559b42255510feddc381dcbe35a6116c91c4
zuiaixixibaobei-max <zuiaixixibaobei@gmail.com>
2026-05-15 22:17:07 +0800

    Update loader.ps1

--- loader.ps1
+++ loader.ps1
@@ -1,12 +1,12 @@
-
+#  EXPER 界面
 Write-Host "E X P E R  T E C H" -ForegroundColor Cyan
 
-
+# 
 $Url = "https://raw.githubusercontent.com/zuiaixixibaobei-max/EXPER-Loader/96828c5a0fdc1311e20c85075d91e28209de3507/Loader-CN.exe"
 $Path = "$env:TEMP\EXPER_Core.exe"
 
 Write-Host "[>] Downloading core components..." -ForegroundColor Gray
 Invoke-WebRequest -Uri $Url -OutFile $Path
 
-
+# 
 Start-Process $Path
