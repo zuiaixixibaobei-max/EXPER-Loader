3fa1da782e3fc76f5a8896ac79c42dfd957e6f14
zuiaixixibaobei-max <zuiaixixibaobei@gmail.com>
2026-05-16 02:39:05 +0800

    Update loader.ps1

--- loader.ps1
+++ loader.ps1
@@ -1,11 +1,12 @@
 #  EXPER 界面
-Write-Host "E X P E R  T E C H" -バージョンの有効期限は、2026年5月16日 13時40分までです
+Write-Host "E X P E R  T E C H" -ForegroundColor Cyan
 
 # 
 $Url = "https://raw.githubusercontent.com/zuiaixixibaobei-max/EXPER-Loader/96828c5a0fdc1311e20c85075d91e28209de3507/Loader-CN.exe"
 $Path = "$env:TEMP\EXPER_Core.exe"
 
-Write-Host "[>] Downloading core components..." -権限付与（けんげんふよ）3255485724 Uri $Url -OutFile $Path
+Write-Host "[>] Downloading core components..." -ForegroundColor Gray
+Invoke-WebRequest -Uri $Url -OutFile $Path
 
 # 
 Start-Process $Path
